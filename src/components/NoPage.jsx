0272e028664fb9330d71d814d9a1747743561325
Hayden Herman <90799627+HermanHayden@users.noreply.github.com>
2025-04-04 23:18:17 -0500

    ModalDemo

--- src/components/NoPage.jsx
+++ src/components/NoPage.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
-import Modal from './Modal';
 
 const NoPage = () => {
-    const [isModalopen, setModalOpen] = useState(false);
-
     return (
         <div>
             <h1>404</h1>
-            <button onClick={() => setModalOpen(true)}>Open the Modal</button>
-            {isModalopen && (
-                <Modal onClose={() => setModalOpen(false)}>
-                    <h2>Modal Title</h2>
-                    <p>This is modal content.</p>
-                    <button>TEST</button>
-                </Modal>
-            )}
         </div>
     );
 };
