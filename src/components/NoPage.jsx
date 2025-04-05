90e2794b97acfb0879586f684a1f22a13dfebcf1
Hayden Herman <90799627+HermanHayden@users.noreply.github.com>
2025-04-04 23:16:59 -0500

    Basic Modal

--- src/components/NoPage.jsx
+++ src/components/NoPage.jsx
@@ -1,5 +1,22 @@
+import React, { useState } from 'react';
+import Modal from './Modal';
+
 const NoPage = () => {
-    return <h1>404</h1>;
+    const [isModalopen, setModalOpen] = useState(false);
+
+    return (
+        <div>
+            <h1>404</h1>
+            <button onClick={() => setModalOpen(true)}>Open the Modal</button>
+            {isModalopen && (
+                <Modal onClose={() => setModalOpen(false)}>
+                    <h2>Modal Title</h2>
+                    <p>This is modal content.</p>
+                    <button>TEST</button>
+                </Modal>
+            )}
+        </div>
+    );
 };
 
 export default NoPage;
