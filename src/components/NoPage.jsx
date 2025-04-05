1f90d0eb5a31fc97c6cbb7db27805ee7ecc357c0
Hayden Herman <90799627+HermanHayden@users.noreply.github.com>
2025-04-04 22:37:10 -0500

    TEST

--- src/components/NoPage.jsx
+++ src/components/NoPage.jsx
@@ -1,5 +1,30 @@
+import React, { Component, useState } from "react";
+import WheelComponent from 'react-wheel-of-prizes';
+
 const NoPage = () => {
-    return <h1>404</h1>;
+    const segments = ['Prize 1', 'Prize 2', 'Prize 3', 'Prize 4', 'Prize 5'];
+    const segColors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF'];
+    const onFinished = (winner) => {
+        alert(`You won: ${winner}`);
+    };
+    return(
+        <div style={{ width: 500, margin: '0 auto', marginTop: '50px' }}>
+            <WheelComponent
+                segments={segments}
+                segColors={segColors}
+                winningSegment=""
+                onFinished={(winner) => onFinished(winner)}
+                primaryColor="black"
+                contrastColor="white"
+                buttonText="Spin"
+                isOnlyOnce={false}
+                size={290}
+                upDuration={100}
+                downDuration={1000}
+                fontFamily="Arial"
+            />
+        </div>
+    );
 };
 
 export default NoPage;
