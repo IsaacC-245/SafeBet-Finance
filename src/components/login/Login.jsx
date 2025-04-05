be5726d0c05f9d6e7c1dea89d1b37eba7242edbd
RyanEbsen <77689652+RyanEbsen@users.noreply.github.com>
2025-04-05 00:51:36 -0500

    fixed login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -16,11 +16,13 @@ const Login = () => {
         return (
             <div className="text-center">
                 <form className="form-signin" onSubmit={(e) => login(username, e)}>
-                    <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                    <input className="form-control" placeholder="username" required={true}
-                           autoFocus="" onChange={e => setUsername(e.target.value)}/>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required={true}/>
-                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                    <h1 className="h3 mb-3 font-weight-normal">Log in</h1>
+                    <label className="sr-only">Username</label>
+                    <input className="form-control" required={true}
+                           onChange={e => setUsername(e.target.value)} />
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" required={true} />
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
                 </form>
             </div>
         );
